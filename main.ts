f382b5fc986c0e9433024f16849a2aed687006ac
knauser <knauser@posteo.net>
2025-11-06 09:14:07 +0100

    Added bounce rest handling.

--- main.ts
+++ main.ts
@@ -7,19 +7,26 @@ scene.onHitWall(SpriteKind.Player, function (sprite, location) {
         sprite.vy = 0 - sprite.vy * friction
         sprite.vx = sprite.vx * friction
     }
-    if (Math.abs(sprite.vx) < 10) {
+    if (Math.abs(sprite.vx) < minBounceSpeed && Math.abs(sprite.vy) < minBounceSpeed) {
         sprite.vx = 0
-        if (Math.abs(sprite.vy) < 10) {
-            sprite.vy = 0
-        }
+        sprite.vy = 0
+        sprite.setBounceOnWall(false)
+    } else {
+        sprite.setBounceOnWall(true)
     }
 })
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.setVelocity(randint(-1000, 1000), randint(-1000, 1000))
+})
+let mySprite: Sprite = null
+let minBounceSpeed = 0
 let friction = 0
 friction = 0.8
+minBounceSpeed = 25
 scene.setBackgroundColor(12)
 tiles.setCurrentTilemap(tileUtil.createSmallMap(tilemap`Level1`))
 tileUtil.setWalls(assets.tile`myTile`, true)
-let mySprite = sprites.create(img`
+mySprite = sprites.create(img`
     . . . . . . . . 
     . f f f f f . . 
     . f 5 5 5 f . . 
